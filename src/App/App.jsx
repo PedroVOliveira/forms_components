d1ecd7a82164dc5e0c091fddd6bb2c43c749c60f
Pedro <pedrovoliveiracontato@gmail.com>
2020-12-19 13:42:38 -0300

    feat(form_components): adicionando componente radio

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { palette } from "../settings";
+import { ThemeProvider } from "styled-components";
+import Radio from "../components/Radio";
 
-function App() {
-  return <h1>teste</h1>;
-}
+const App = () => {
+  const theme = { palette };
+  const [color, setColor] = React.useState("Vermelho");
+  return (
+    <ThemeProvider theme={theme}>
+      <h2>Componente Radio</h2>
+      <Radio options={["Azul", "Vermelho"]} value={color} setValue={setColor} />
+    </ThemeProvider>
+  );
+};
 
 export default App;
