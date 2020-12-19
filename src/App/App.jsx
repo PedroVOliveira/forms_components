79f4161ac924b599a546ccfd1bec29c8fcb00b4f
Pedro <pedrovoliveiracontato@gmail.com>
2020-12-19 14:06:24 -0300

    feat(form_components): adicionando componente checkbox

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -2,14 +2,22 @@ import React from "react";
 import { palette } from "../settings";
 import { ThemeProvider } from "styled-components";
 import Radio from "../components/Radio";
+import Checkbox from "../components/Checkbox";
 
 const App = () => {
   const theme = { palette };
   const [color, setColor] = React.useState("Vermelho");
+  const [language, setLanguage] = React.useState("");
   return (
     <ThemeProvider theme={theme}>
       <h2>Componente Radio</h2>
       <Radio options={["Azul", "Vermelho"]} value={color} setValue={setColor} />
+      <h2>Componente Checkbox</h2>
+      <Checkbox
+        options={["Javascript", "PHP", "Python"]}
+        value={language}
+        setValue={setLanguage}
+      />
     </ThemeProvider>
   );
 };
